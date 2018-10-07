bb9dc5dbf6f8143a2c7907a6d715441c3a1e41fa
Dennis Prochko <wolfsoft@mail.ru>
2018-10-07 18:33:08 +0300

    clear settings on uninstall

--- src/uninstall.php
+++ src/uninstall.php
@@ -12,3 +12,10 @@
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
+
+delete_option( 'botguard_enabled' );
+delete_site_option( 'botguard_enabled' );
+delete_option( 'botguard_server_primary' );
+delete_site_option( 'botguard_server_primary' );
+delete_option( 'botguard_server_secondary' );
+delete_site_option( 'botguard_server_secondary' );
